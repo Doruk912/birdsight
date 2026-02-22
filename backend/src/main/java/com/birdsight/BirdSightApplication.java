cc7a77b04af998c3e5abf5f02cd03da7ba0b0902
Doruk Ege Aksu <82142325+Doruk912@users.noreply.github.com>
2026-02-22 16:17:53 +0300

    backend: spring file structure

---
+++ backend/src/main/java/com/birdsight/BirdSightApplication.java
@@ -0,0 +1,13 @@
+package com.birdsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdSightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdSightApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/birdsight/BirdSightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birdsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
